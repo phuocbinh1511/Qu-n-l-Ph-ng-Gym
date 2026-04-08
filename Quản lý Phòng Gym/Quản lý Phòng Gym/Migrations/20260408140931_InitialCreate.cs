bb3602c06aa81311afe4479dabad1f14a131f650
phuocbinh1511 <binhbro1511@gmail.com>
2026-04-08 21:12:39 +0700

    Bước 5: Chạy lệnh Migration thành công, hoàn thành bài tập Code First

---
+++ Quản lý Phòng Gym/Quản lý Phòng Gym/Migrations/20260408140931_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Quản_lý_Phòng_Gym.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GoiTaps",
+                columns: table => new
+                {
+                    MaGoi = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenGoi = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GiaTien = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GoiTaps", x => x.MaGoi);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HoiViens",
+                columns: table => new
+                {
+                    MaHoiVien = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HoTen = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SoDienThoai = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    MaGoi = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HoiViens", x => x.MaHoiVien);
+                    table.ForeignKey(
+                        name: "FK_HoiViens_GoiTaps_MaGoi",
+                        column: x => x.MaGoi,
+                        principalTable: "GoiTaps",
+                        principalColumn: "MaGoi",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HoiViens_MaGoi",
+                table: "HoiViens",
+                column: "MaGoi");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HoiViens");
+
+            migrationBuilder.DropTable(
+                name: "GoiTaps");
+        }
+    }
+}
